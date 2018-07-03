d54d2271232bfa55213b5576fd31b8acb21270a4
luowenzhuo <luowenzhuo@lbesec.org>
2018-07-03 18:08:39 +0800

    [add]最笨的聊天机器人

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 18-7-3
+@Author  : leemiracle
+"""
+import time
+
+
+def main():
+    while True:
+        input("haha: ")
+        time.sleep(1)
+        print("leemiracle: {}".format("笑什么"))
+
+
+if __name__ == '__main__':
+    main()
